6075aa837f8a3c65a859bafbf9b2b582f4d4b18f
marceloteclas <mdj3114@gmail.com>
2025-07-22 17:03:22 -0300

    Envio Home Inicial

--- marketplace-ifba/src/App.jsx
+++ marketplace-ifba/src/App.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import NavLeft from './Components/Navigation/NavLeft';
 import Header from './Components/Header/Header';
 import Overview from './Components/Overview';
 import Demands from './Components/Demands';
-import Proposals_ from './Components/Proposals_'; Conversations
+import Proposals_ from './Components/Proposals_';
 import GroupResearch from './Components/GroupResearch';
 import Conversations from './Components/Conversations';
+import Home from './Components/Home';
 
-function App() {
-  return <div className='grid-container'>
-    <BrowserRouter>
-      <NavLeft />
-      <Header />
+
+function AppContent() {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
+  return (
+    <div className={isHome ? '' : 'grid-container'}>
+      {!isHome && <NavLeft />}
+      {!isHome && <Header />}
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/overview" element={<Overview />} />
         <Route path="/demands" element={<Demands />} />
         <Route path="/proposals" element={<Proposals_ />} />
         <Route path="/research-group" element={<GroupResearch />} />
         <Route path="/conversations" element={<Conversations />} />
       </Routes>
-    </BrowserRouter>
+    </div>
+  );
+}
 
-  </div>
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
 }
 
 export default App;
